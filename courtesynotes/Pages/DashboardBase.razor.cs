8d7365683f4d6ceae17e7012c441b1ef3a5c9244
Mr-Courtesy <49980632+Mr-Courtesy@users.noreply.github.com>
2021-06-16 13:21:14 -0400

    Add the dashboard page and its base

---
+++ courtesynotes/Pages/DashboardBase.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazored.SessionStorage;
+
+namespace courtesynotes.Pages
+{
+    public class DashboardBase : ComponentBase
+    {
+        [Inject] protected ISessionStorageService StorageService { get; set; }
+        protected string SessionEmail;
+        protected override async Task OnInitializedAsync()
+        {
+            SessionEmail = await StorageService.GetItemAsync<string>("_username");
+        }
+    }
+}

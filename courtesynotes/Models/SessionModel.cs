36210ae0d6defabdb8de518749fdcdc0fd7d8015
Mr-Courtesy <49980632+Mr-Courtesy@users.noreply.github.com>
2021-06-16 13:20:54 -0400

    Add SessionModel to determin authorized status of the session

---
+++ courtesynotes/Models/SessionModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace courtesynotes.Models
+{
+    public static class SessionModel
+    {
+        public static bool Authorized { get; set; } = false;
+    }
+}

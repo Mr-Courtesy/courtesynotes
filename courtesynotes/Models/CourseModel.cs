700e7c36856922351190a4bc7781356cf5b3fdf8
Mr-Courtesy <49980632+Mr-Courtesy@users.noreply.github.com>
2021-06-17 14:45:12 -0400

    Add the model for a course

---
+++ courtesynotes/Models/CourseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace courtesynotes.Models
+{
+    public class CourseModel
+    {
+        [Required]
+        [MaxLength(length: 16, ErrorMessage = "Course/subject name cannot be more than 16 characters long.")]
+        public string Name { get; set; }
+    }
+}

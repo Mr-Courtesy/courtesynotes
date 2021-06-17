47b7a353429f9b69a7026316aac24e881f67e80e
Mr-Courtesy <49980632+Mr-Courtesy@users.noreply.github.com>
2021-06-17 14:46:28 -0400

    Add logic for opening the CourseComponent modal

--- courtesynotes/Pages/DashboardBase.razor.cs
+++ courtesynotes/Pages/DashboardBase.razor.cs
@@ -4,16 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Blazored.SessionStorage;
+using courtesynotes.Data;
+using Google.Cloud.Firestore;
+using courtesynotes.Models;
+using Blazored.Modal.Services;
+using Blazored.Modal;
+using courtesynotes.Components;
 
 namespace courtesynotes.Pages
 {
     public class DashboardBase : ComponentBase
     {
+        [Inject] protected IModalService Modal { get; set; }
         [Inject] protected ISessionStorageService StorageService { get; set; }
+        [Inject] protected IModalService ModalService { get; set; }
         protected string SessionEmail;
+        protected string CourseName;
         protected override async Task OnInitializedAsync()
         {
             SessionEmail = await StorageService.GetItemAsync<string>("_username");
+        }
+        protected async Task AddCourse()
+        {
+            Modal.Show<CourseComponent>("Add a course");
         }
     }
 }

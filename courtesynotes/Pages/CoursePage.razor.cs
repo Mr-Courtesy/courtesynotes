389bed1a692197a2b99cffd456f83987413cb6ee
Mr-Courtesy <49980632+Mr-Courtesy@users.noreply.github.com>
2021-06-17 14:45:51 -0400

    Add the base for the course page

---
+++ courtesynotes/Pages/CoursePage.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace courtesynotes.Pages
+{
+    public partial class CoursePage : ComponentBase
+    {
+        public string course { get; set; }
+    }
+}
